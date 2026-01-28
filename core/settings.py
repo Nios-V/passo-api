a507150b3533e7e38093fe87d2e52c8d406ed2c5
Nicolás Cáceres <niosv.1104@gmail.com>
2026-01-28 11:21:21 -0300

    feat: users generics; JWT implementation

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'tickets',
+    'users',
 ]
 
 REST_FRAMEWORK = {
@@ -130,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'users.User'
